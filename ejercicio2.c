37a8671247f65c5438849382809c50f4561b8b2b
Seiji314288758 <nietoseiji@gmail.com>
2025-10-17 13:42:18 -0600

    ejercicio2.c

---
+++ ejercicio2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+void main()
+{
+  int num1, num2, res;
+
+  printf("Escribe el primer numero: \n");
+  scanf("%d", %num1);
+  printf("Escribe el segundo numero: \n");
+  scanf("%d", %num2);
+
+  if(num1 % num2 == 0)
+{
+  printf("El numero es divisible \n");
+}
+else
+{
+  printf("El numero no es divisible \n");
+}
+}
