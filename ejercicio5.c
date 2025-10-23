237acdbc567e148a58c68de0047bbe2127d6f98b
Seiji314288758 <nietoseiji@gmail.com>
2025-10-23 14:51:10 -0600

    Add ticket pricing program based on age

---
+++ ejercicio5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+  const float PRECIO_ENTRADA = 7.0; // Precio base de la entrada
+  int edad;
+  float precio_final;
+
+  printf("Ingresa tu edad: ");
+  scanf("%d", &edad);
+
+  if (edad < 5)
+  {
+   precio_final = PRECIO_ENTRADA * 0.40;  // 60% de descuento → paga el 40%
+  }
+  else if (edad > 60)
+  {
+   precio_final = PRECIO_ENTRADA * 0.45; // 55% de descuento → paga el 45%
+  }
+  else 
+  {
+   precio_final = PRECIO_ENTRADA; // Sin descuento
+  }
+
+  printf("El precio de su entrada es: %.2f Euros\n", precio_final);
+
+  return 0;
+}
