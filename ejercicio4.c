b6023fe6d9e656f5866431f312787c5000f9c3a9
Seiji314288758 <nietoseiji@gmail.com>
2025-10-23 14:27:57 -0600

    ejercicio4.c

---
+++ ejercicio4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+
+{
+    int num1, num2;
+
+    printf("Eescribe el primer numero entero: ");
+    scanf("%d", &num1);
+
+    printf("Escribe el segundo numero entero: ");
+    scanf("%d", &num2);
+
+    if(num1 > num2) 
+    {
+       printf("El primer numero (%d) es mayor que el segundo (%d).\n", num1, num2);
+    } 
+    else if (num2 > num1)
+    {
+       printf("El segundo numero (%d) es mayor que el primero (%d).\n", num2, num1);
+    }
+    else 
+    {
+       printf("Ambos numeros son iguales (%d). \n", num1);
+    }
+
+    return 0;
+}
