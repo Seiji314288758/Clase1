27ecfb7f9641e590d61bdf8d684a551915bb2b5c
Seiji314288758 <nietoseiji@gmail.com>
2025-10-24 03:39:11 -0600

    Add salary calculation program in ejercicio14.c

---
+++ ejercicio14.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main ()
+{
+  float horas, pago_hora, salario;
+
+  printf("Ingrese las horas trabajadas en la semana: ");
+  scanf("%f", &horas);
+
+  printf("Ingrese el pago por hora: ");
+  scanf("%f", &pago_hora);
+
+  if (horas <= 40)
+   {
+    salario = horas * pago_hora;
+   }
+  else
+    if (horas <= 50)
+    {
+     salario = (40 * pago_hora) + ((horas - 40) * pago_hora * 1.5);
+    }
+else 
+    {
+     salario = (40 * pago_hora) + (10 * pago_hora * 1.5) + ((horas - 50) * pago_hora * 2);
+    }
+printf("\nEl salario semanal es: %.2f\n", salario);
+
+return 0;
+
+}
