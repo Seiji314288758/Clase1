ea6dd4cd9b1348f32042b411778fc4eaddbf0509
Seiji314288758 <nietoseiji@gmail.com>
2025-10-24 02:15:11 -0600

    Add number sorting program in ejercicio10.c
    Implement a program to sort three numbers in descending order.

---
+++ ejercicio10.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main ()
+{
+  int a, b, c;
+  int mayor, medio, menor;
+
+  printf("Ingrese el primer número: ");
+  scanf("%d", &a);
+
+  printf("Ingrese el segundo número: ");
+  scanf("%d", &b);
+
+  printf("Ingrese el tercer número: ");
+  scanf("%d", &c);
+
+  //Determinar el orden de los números
+  if (a >= b && a >= c) 
+  {
+   mayor = a;
+   if (b >= c) 
+    {
+     medio = b;
+     menor = c;
+    }
+   else 
+    {
+     medio = c;
+     menor = a;
+    }
+  }
+   else
+    {
+     mayor = c;
+     if (a >= b)
+      {
+      medio = a;
+      menor = b;
+      }
+     else
+      { 
+       medio = b;
+       menor = a;
+      }
+    }
+ printf("\nLos números ordenados de mayor a menor son: %d, %d, %d\n", mayor, medio, menor);
+
+ return 0;
+
+}
+
+   
