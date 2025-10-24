4455a3c0f80740a57c21c74260dde6163f9c7bbe
Seiji314288758 <nietoseiji@gmail.com>
2025-10-24 01:44:22 -0600

    Implement program to find the smallest of five numbers
    This program prompts the user to input five numbers and determines the smallest among them.

---
+++ ejercicio9.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+  int i, numero;
+  int menor;
+
+  printf("Ingrese el número 1. ");
+  scanf("%d", &menor); //El primero se guarda como referencia inicial
+
+  //Leer los 4 números restantes
+  for (i = 2; i <= 5; i++)
+    {
+     printf("Ingrese el número %d: ", i);
+     scanf("%d", &numero);
+
+     if (numero < menor)
+      {
+        menor = numero; //Actualiza el menor si encuentra uno más pequeño
+      }
+    }
+
+    printf("\nEl número menor es: %d\n", menor);
+
+    return 0;
+}
