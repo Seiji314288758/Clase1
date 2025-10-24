2971f057571d7b3947bdf4fbf089e5c8187dad11
Seiji314288758 <nietoseiji@gmail.com>
2025-10-24 03:54:24 -0600

    Implement card number identification in ejercicio15.c

---
+++ ejercicio15.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+  int numero;
+
+  printf("Ingrese el numero de la carta (1 al 12): ");
+  scanf("%d", &numero);
+
+  switch (numero)
+   {
+    case 1:
+       printf("As\n");
+       break;
+    case 2:
+       printf("Sota\n");
+       break;
+    case 3:
+       printf("Caballo\n");
+       break;
+    case 4: 
+       printf("Rey\n");
+       break;
+    default:
+       if (numero >= 2 && numero <= 9)
+        {
+         printf("No es as ni figura\n");
+        }
+    else
+       {
+        printf("Este no es un número de una carta de la baraja española\n");
+       }
+   }
+return 0;
+}
